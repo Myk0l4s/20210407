c7b9b6b736cfe53d27325559aac532f839384c91
Myk0l4s <morkunas.mykolas@gmail.com>
2021-04-28 20:08:06 +0300

    document body

--- js/main.js
+++ js/main.js
@@ -5,21 +5,42 @@
 let burger = document.querySelector('.burger');
 let navigation = document.querySelector('.nav');
 
-burger.addEventListener('click', () => {
+
+if (burger) {
+    burger.addEventListener('click', () => {
     
-    if (navigation.className == 'nav on') {
-        navigation.classList.remove('on');
-    } else {
-        navigation.classList.add('on');
-    }
-    
-});
+        if (navigation.className == 'nav on') {
+            navigation.classList.remove('on');
+        } else {
+            navigation.classList.add('on');
+        }
+        
+    });
+  } else {
+    console.log('elemento nera');
+  }
+
 
 // ------------------------------------------------------
 
 
+let likeBody = document.body;
+
+console.log(likeBody);
+
+likeBody.addEventListener('click', (e) => {
+    console.log(e.path[1]);
+});
 
 
 
 
-
+// burger.addEventListener('click', () => {
+    
+//     if (navigation.className == 'nav on') {
+//         navigation.classList.remove('on');
+//     } else {
+//         navigation.classList.add('on');
+//     }
+    
+// });
